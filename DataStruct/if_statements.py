f5124e2d8aba7390ca491eff407bb4fa13f9c6c7
rickytorrez <torrez.ricky@ymail.com>
2018-07-14 21:32:52 -0400

    Data

---
+++ DataStruct/if_statements.py
@@ -0,0 +1,13 @@
+should_continue = True
+if should_continue:
+    print("Hello")
+
+known_people = ["John", "Anna", "Mary"]
+person = input("Enter the person you know: ")
+
+
+# format lets you substitute things on strings #
+if person in known_people:
+    print("You know {}!".format(person))
+else:
+    print("You don't know {}!".format(person))
