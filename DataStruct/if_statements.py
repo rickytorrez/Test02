eb1ae52028d0b9e5bfae38d93c89e7840bd9bc2f
rickytorrez <torrez.ricky@ymail.com>
2018-07-16 15:31:30 -0400

    Ifs and buts

--- DataStruct/if_statements.py
+++ DataStruct/if_statements.py
@@ -12,3 +12,19 @@ if person in known_people:
     print("You know {}!".format(person))
 else:
     print("You don't know {}!".format(person))
+
+def who_do_you_know():
+    people = input ("Enter the people you know separated by commas: ")      # John,Rolf,Anna,Greg
+    people_list = people.split(",")                                         # ["John","Rolf","Anna","Greg"]
+    people_without_spaces = []
+    for person in people_list:                                              # loop to remove spaces from names
+        people_without_spaces.append(person.strip())
+
+    return people_without_spaces
+
+def ask_user():
+    person = input ("Enter a name of someone you know: ")
+    if person in who_do_you_know():                                         # Grabs the list of the method above
+        print("You konw {}!".format(person))
+
+ask_user()                                                                  # Call the method
