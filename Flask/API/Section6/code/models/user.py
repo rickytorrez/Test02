bc50c630cffc3786c87cc0c101482bc62366e620
rickytorrez <torrez.ricky@ymail.com>
2018-07-23 17:03:26 -0400

    User Model

--- Flask/API/Section6/code/models/user.py
+++ Flask/API/Section6/code/models/user.py
@@ -18,35 +18,12 @@ class UserModel(db.Model):                                                      
 ############################################### FIND USER BY USERNAME ################################################
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')                                         # Initialize the connection
-        cursor = connection.cursor()                                                    # Initialize the cursor
-
-        query = "SELECT * FROM users WHERE username=?"                                  # Query that will search the table for a given username
-        result = cursor.execute(query, (username,))                                     # Get the result set for that found username
-        row = result.fetchone()                                                         # Get the first row
-        if row:                                                                         # If the row exists
-            user = cls(*row)                                                            # Create a User object with the data from that row
-                                                                                        # *row is a set of arguments for id, username and password
-        else:                                                                           # If there's no row
-            user = None                                                                 # user is none
-
-        connection.close()                                                              # Close the connection
-        return user                                                                     # return user or none
+        return cls.query.filter_by(username=username).first()                           # Return the cls(UserModel), start the query builder
+                                                                                        # Filter by username(table name) = username(argument)
+                                                                                        # Return the first row and SQLAlchemy converts it into
+                                                                                        # an object
 
 ################################################ FIND USER BY USE ID #################################################
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')                                         # Initialize the connection
-        cursor = connection.cursor()                                                    # Initialize the cursor
-
-        query = "SELECT * FROM users WHERE id=?"                                        # Query that will search the table for a given id
-        result = cursor.execute(query, (_id,))                                          # Get the result set for that found _id
-        row = result.fetchone()                                                         # Get the first row
-        if row:                                                                         # If the row exists
-            user = cls(*row)                                                            # Create a User object with the data from that row
-                                                                                        # *row is a set of arguments for id, username and password
-        else:                                                                           # If there's no row
-            user = None                                                                 # user is none
-
-        connection.close()                                                              # Close the connection
-        return user
+        return cls.query.filter_by(id=_id).first()                                      # Similar to the method above
