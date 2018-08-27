f8a1b10388a49bcecbceb83f9dae6bb50317f794
rickytorrez <torrez.ricky@ymail.com>
2018-08-27 16:28:37 -0400

    Back at it

---
+++ Django/ShoppingCart/myenvironment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ricky/Documents/Coding/Python2.0/Django/ShoppingCart/myenvironment/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
