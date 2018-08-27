54f1c2a47094eb8235411fefc2ea2614363a968a
rickytorrez <torrez.ricky@ymail.com>
2018-08-27 17:06:12 -0400

    Synchronized Database, File Upload Uploading and Pillow Extension

--- Django/ShoppingCart/myenvironment/perfectcushion/perfectcushion/settings.py
+++ Django/ShoppingCart/myenvironment/perfectcushion/perfectcushion/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')          #Where our static files are going to be saved
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'staic','media')
--- Django/ShoppingCart/myenvironment/perfectcushion/shop/models.py
+++ Django/ShoppingCart/myenvironment/perfectcushion/shop/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
 	description = models.TextField(blank=True)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 	price = models.DecimalField(max_digits=10, decimal_places=2)
-	image = models.ImageField(upload_to='product', blank=unique)
+	image = models.ImageField(upload_to='product', blank=True)
 	stock = models.IntegerField()
 	available = models.BooleanField(default=True)
 	created = models.DateTimeField(auto_now_add=True)
